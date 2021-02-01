7c8a28bd784cdeca363f93d0000a2a29ca3da6c1
semanurtopal <77543320+semanurtopal@users.noreply.github.com>
2021-02-01 18:33:59 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,73 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+                new Car {Id=1, BrandId=1, ColorId=1, ModelYear=2015, DailyPrice=200, Description="Economic" },
+                new Car {Id=2, BrandId=2, ColorId=2, ModelYear=2018, DailyPrice=300, Description="Comfort" },
+                new Car {Id=3, BrandId=3, ColorId=1, ModelYear=2020, DailyPrice=500, Description="Premium" },
+                new Car {Id=4, BrandId=4, ColorId=3, ModelYear=2020, DailyPrice=400, Description="Prestige" },
+                new Car {Id=5, BrandId=5, ColorId=2, ModelYear=2009, DailyPrice=200, Description="Economic" },
+
+                };
+
+            List<Brand> _brands;
+            _brands= new List<Brand>{
+                new Brand {BrandId=1, BrandName="Fiat" },
+                new Brand {BrandId=2, BrandName="Peugeot" },
+                new Brand {BrandId=3, BrandName="Mercedes" },
+                new Brand {BrandId=4, BrandName="Opel" },
+                new Brand {BrandId=5, BrandName="Renault" }
+            };
+
+            List<Color> _colors;
+            _colors = new List<Color>{
+                new Color {ColorId=1, ColorName="Grey" },
+                new Color {ColorId=2, ColorName="White" },
+                new Color {ColorId=3, ColorName="Black" },
+                
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c=>c.Id==id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId= car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
